b7e71bada593e2457e1976b8f1407737f1898425
BoGoBooM <myPikcha@gmail.com>
2025-09-12 17:09:00 +0300

    task soluted

---
+++ src/data/data.ts
@@ -0,0 +1,23 @@
+export type Product = {
+  id: number;
+  name: string;
+  price: number;
+};
+
+export const products: Product[] = [
+  {
+    id: 1,
+    name: "Apple",
+    price: 15,
+  },
+  {
+    id: 2,
+    name: "Orange",
+    price: 20,
+  },
+  {
+    id: 3,
+    name: "Banana",
+    price: 12,
+  },
+];
