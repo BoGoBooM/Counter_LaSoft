175099ae3a723773507626c198e6556c46bc9d7a
BoGoBooM <myPikcha@gmail.com>
2025-09-19 18:13:27 +0300

    use reduce & themes were added

---
+++ src/themes/themes.ts
@@ -0,0 +1,20 @@
+export const themes = {
+  light: {
+    background: "#FFF",
+    cardBG: "#F0F4F8",
+    textFirst: "#1A1A1A",
+    textSecond: "#293241",
+    textThird: "#0D3B66",
+    button: "#3D5A80",
+    buttonText: "#FFF"
+  },
+  dark: {
+    background: "#222",
+    cardBG: "#333",
+    textFirst: "#1A1A1A",
+    textSecond: "#EBE7E6",
+    textThird: "#F0F8FF",
+    button: "#495057",
+    buttonText: "#EBEBEB"
+  }
+};
